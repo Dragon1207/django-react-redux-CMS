97cb4d3d42962a83c9f207500b46e9f184c021f1
Dragon1207 <smileboth0805@outlook.com>
2017-10-10 23:53:27 -0700

    strong typed service

--- client/src/app/shared/post.service.ts
+++ client/src/app/shared/post.service.ts
@@ -2,23 +2,26 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from '@app/djudo.token';
 import { Observable } from 'rxjs/Observable';
-import { Post } from '../post-list/posts';
+import { Post, Category } from '../post-list/posts';
 
 @Injectable()
 export class PostsService {
 
   constructor(private http: HttpClient, @Inject(API_URL) private apiUrl) { }
 
-  getPosts(): Observable<Object> {
-    return this.http.get(`${this.apiUrl}/posts/`)
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.apiUrl}/posts/`)
+      .map(res => res);
   }
 
-  getPost(id): Observable<Object> {
-    return this.http.get(`${this.apiUrl}/posts/${id}/`);
+  getPost(id): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/posts/${id}/`)
+      .map(res => res);
   }
 
-  getCategories(): Observable<Object> {
-    return this.http.get(`${this.apiUrl}/categories/`);
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.apiUrl}/categories/`)
+      .map(res => res);
   }
 
 }

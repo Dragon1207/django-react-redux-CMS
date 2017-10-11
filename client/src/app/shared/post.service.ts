1c2c58bbf21bfe30f0c654c63439bc61a5adb345
Dragon1207 <smileboth0805@outlook.com>
2017-10-10 23:09:31 -0700

    post.service typed

--- client/src/app/shared/post.service.ts
+++ client/src/app/shared/post.service.ts
@@ -1,21 +1,23 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from '@app/djudo.token';
+import { Observable } from 'rxjs/Observable';
+import { Post } from '../post-list/posts';
 
 @Injectable()
 export class PostsService {
 
   constructor(private http: HttpClient, @Inject(API_URL) private apiUrl) { }
 
-  getPosts() {
-    return this.http.get(`${this.apiUrl}/posts/`);
+  getPosts(): Observable<Object> {
+    return this.http.get(`${this.apiUrl}/posts/`)
   }
 
-  getPost(id) {
+  getPost(id): Observable<Object> {
     return this.http.get(`${this.apiUrl}/posts/${id}/`);
   }
 
-  getCategories() {
+  getCategories(): Observable<Object> {
     return this.http.get(`${this.apiUrl}/categories/`);
   }
 

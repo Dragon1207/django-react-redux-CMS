f201d6dd54f64edff2ee58aa0adabee1215dc38b
Dragon1207 <smileboth0805@outlook.com>
2017-10-16 23:00:46 -0700

    removed comments

--- server/blog/api/urls.py
+++ server/blog/api/urls.py
@@ -11,14 +11,6 @@ from blog.api.views import (
 urlpatterns = [
     url(r'^posts/$', PostViewSet.as_view(), name='list'),
     url(r'^categories/$', CategoryViewApi.as_view(), name='categories'),
-    # url(r'^create/$', PostCreateAPIView.as_view(), name='create'),
     url(r'^posts/slug/(?P<slug>[\w-]+)/$', PostSlugAPIView.as_view(), name='slug'),
     url(r'^posts/(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
-    # url(r'^(?P<slug>[\w-]+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
-    # url(r'^(?P<slug>[\w-]+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 ]
-
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'posts', views.PostViewSet)
-# router.register(r'categories', views.CategoryViewApi)

f934c259febc82ac04cd0857c87bfbc84b8af8fc
Dragon1207 <smileboth0805@outlook.com>
2017-10-18 17:03:16 -0700

    17: comments models and api created

---
+++ server/accounts/views.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+
+    )
+from django.shortcuts import render, redirect
+
+from .forms import UserLoginForm, UserRegisterForm
+
+def login_view(request):
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+
+
+def register_view(request):
+    print(request.user.is_authenticated())
+    next = request.GET.get('next')
+    title = "Register"
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
--- server/blog/api/serializers.py
+++ server/blog/api/serializers.py
@@ -2,6 +2,10 @@ from blog.models import Post, Category
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from accounts.api.serializers import UserDetailSerializer
+from comments.api.serializers import CommentSerializer
+from comments.models import Comment
+
 post_detail_url = serializers.HyperlinkedIdentityField(
         view_name='posts-api:detail',
         lookup_field='pk'
@@ -30,9 +34,22 @@ class PostSerializer(serializers.ModelSerializer):
     url = post_detail_url
     category_obj = CategorySerializer(source='category')
     image = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
     def get_image(self, instance):
         # returning image url if there is an image else blank string
         return instance.image.url if instance.image else ''
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
+
+    def get_comments(self, obj):
+        c_qs = Comment.objects.filter_by_instance(obj)
+        comments = CommentSerializer(c_qs, many=True).data
+        return comments
+
     class Meta:
         model = Post
         fields = (
@@ -46,8 +63,10 @@ class PostSerializer(serializers.ModelSerializer):
             'body',
             'published',
             'created',
-            'category_obj'
+            'category_obj',
+            'comments'
         )
+
 
 class PostSlugSerializer(serializers.ModelSerializer):
     url = post_slug_url

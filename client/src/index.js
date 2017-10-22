243d511bb2f8e0b9712447214f56d64257bfe590
Dragon1207 <smileboth0805@outlook.com>
2017-10-22 02:16:03 -0700

    18: added router

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import registerServiceWorker from './registerServiceWorker';
+import { BlogApp } from './reducers'
+
 import './index.css';
 import './assets/styles/styles.css';
 import { App } from './App';
-import { BlogApp } from './reducers'
-import registerServiceWorker from './registerServiceWorker';
+import { PostList } from './components/posts/post-list/'
 
 let appStore = createStore(BlogApp)
 
 ReactDOM.render(
   <Provider store={appStore}>
-    <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>
+    <BrowserRouter>
+      <MuiThemeProvider>
+        <Route path="/" component={App} />
+        <Route path="/posts" component={PostList} />
+      </MuiThemeProvider>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );

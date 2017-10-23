efd170b31118f4af4601449691f5466df4fc6411
Dragon1207 <smileboth0805@outlook.com>
2017-10-23 13:29:20 -0700

    18: broke header down into smaller components

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -7,6 +7,38 @@ import MenuItem from 'material-ui/MenuItem';
 import PostList from '../../containers/posts/post-list/';
 import { App } from '../../App';
 
+function Icon(props) {
+  return (
+    <i onClick={props.handleToggle} className={`material-icons ${props.name}`}>
+      {props.name}
+    </i>
+  )
+}
+
+function Menu(props) {
+  return (
+    <Drawer className={'drawer'} open={props.open}>
+      <Icon handleToggle={props.handleToggle} name={'close'} />
+      <Link to="/"><MenuItem onClick={props.handleToggle}>Home</MenuItem></Link>
+      <Link to="/posts"><MenuItem onClick={props.handleToggle}>Posts</MenuItem></Link>
+    </Drawer>
+  )
+}
+
+function HeaderComponent(props) {
+  return (
+    <header>
+      <Menu handleToggle={props.handleToggle} open={props.open} />
+      <div className={'header-container'}>
+        <Icon handleToggle={props.handleToggle} name={'menu'} />
+        <Link to="/"><h1 className={'title'}>Djudo</h1></Link>
+        <h2>the next generation of cms</h2>
+        <button>Get Started</button>
+      </div>
+    </header>
+  )
+}
+
 class Header extends React.Component {
 
   constructor(props) {
@@ -17,21 +49,7 @@ class Header extends React.Component {
   handleToggle = () => this.setState({ open: !this.state.open });
 
   render() {
-    return (
-      <header>
-          <Drawer className={'drawer'} open={this.state.open}>
-            <i onClick={this.handleToggle} className={"material-icons close"}>close</i>
-            <Link to="/"><MenuItem onClick={this.handleToggle}>Home</MenuItem></Link>
-            <Link to="/posts"><MenuItem onClick={this.handleToggle}>Posts</MenuItem></Link>
-          </Drawer>  
-        <div className={'header-container'}>
-          <i onClick={this.handleToggle} className={"material-icons menu"}>menu</i>
-          <Link to="/"><h1 className={'title'}>Djudo</h1></Link>
-          <h2>the next generation of cms</h2>
-          <button>Get Started</button>
-        </div>
-      </header>
-    )
+    return <HeaderComponent handleToggle={this.handleToggle} open={this.state.open} />
   }
 }
 

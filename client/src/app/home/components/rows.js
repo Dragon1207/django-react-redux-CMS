debb5536f20cd2639f07f45afdce5d4d0f15d27a
Dragon1207 <smileboth0805@outlook.com>
2017-10-23 14:39:26 -0700

    18: refactored home into smaller components

---
+++ client/src/app/home/components/rows.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import timelapseFrameImg from '../../../assets/images/built-for-speed-image/timelapse-frame.svg';
+
+function RowLabel(props) {
+	return (
+		<div className={"speed-text label"} >{ props.children }</div>
+	)
+}
+
+function RowDesc(props) {
+	return (
+		<div className={"speed-text description"} >{ props.children }</div>
+	)
+}
+
+export function TimeLapseImg() {
+	return (
+		<div className={"speed-image-container scale"}>
+			<img src={timelapseFrameImg} />
+			<i className={"material-icons timelapse-icon"}>timelapse</i>
+		</div>
+	)
+}
+
+export function Row(props) {
+	return (
+		<div className={"row-container " + props.class}>
+			{props.children}
+		</div>
+	)
+}
+
+export function RowText(props) {
+	return (
+		<div>
+			<RowLabel>{ props.label }</RowLabel>
+			<RowDesc>{ props.desc }</RowDesc>
+		</div>
+	)
+}
--- client/src/app/home/index.js
+++ client/src/app/home/index.js
@@ -1,58 +1,31 @@
 import React from 'react';
-import timelapseFrameImg from '../../assets/images/built-for-speed-image/timelapse-frame.svg';
 import hybridImg from '../../assets/images/hybrid-app-image.svg';
 import allDevicesImg from '../../assets/images/djudo-tech-devices-image.svg';
+import { config } from './config';
+import { Row, RowText, TimeLapseImg } from './components/rows';
 
-const config = {
-  speed: {
-    label: 'Built for Speed',
-    description: 'Made for flexibility and performance',
-    img: '../../../assets/images/built-for-speed-image/built-for-speed-image@1x.png 1x',
-    img2x: '../../../assets/images/built-for-speed-image/built-for-speed-image@2x.png 2x',
-    timelapseImg: '../../../assets/images/built-for-speed-image/timelapse-icon.svg',
-    timelapseFrameImg: '../../../assets/images/built-for-speed-image/timelapse-frame.svg'
-  },
-  hybrid: {
-    label: 'Hybrid App',
-    description: 'Django handles the engine, Angular handles the beauty',
-    img: '../../../assets/images/hybrid-app-image.svg'
-  },
-  allDevices: {
-    label: 'Mobile to Desktop',
-    description:
-    'Develop applications for all devices in a sinch',
-    img:
-    '../../../assets/images/djudo-tech-devices-image.svg'
-  }
-}
 
 export function Home() {
   return (
     <section className={"home"} >
-      <div className={"row-container flex flex-row"}>
-        <div>
-          <div className={"speed-text label"} >{ config.speed.label }</div>
-          <div className={"speed-text description"} >{ config.speed.description }</div>
-        </div>
-        <div className={"speed-image-container scale"}>
-          <img src={timelapseFrameImg} />
-          <i className={"material-icons timelapse-icon"}>timelapse</i>
-        </div>
-      </div>
-      <div className={"row-container flex flex-row"}>
+      <Row class={"flex-row"}>
+        <RowText
+          label={config.speed.label}
+          desc={config.speed.description} />
+        <TimeLapseImg/>
+      </Row>
+      <Row class={"flex-row"}>
         <img className={"scale"} src={hybridImg} />
-        <div>
-          <div className={"hybrid-text label flex-6"} >{ config.hybrid.label }</div>
-          <div className={"hybrid-text description flex-6"} >{ config.hybrid.description }</div>
-        </div>
-      </div>
-      <div className={"row-container"}>
-        <div>
-          <div className={"allDevices-text label flex-6"} >{ config.allDevices.label }</div>
-          <div className={"allDevices-text description flex-6"} >{ config.allDevices.description }</div>
-        </div>
+        <RowText
+          label={config.hybrid.label}
+          desc={config.hybrid.description}/>
+      </Row>
+      <Row>
+        <RowText
+          label={config.allDevices.label}
+          desc={config.allDevices.description}/>
         <img className={"allDevices-img"} src={allDevicesImg} />
-      </div>
+      </Row>
     </section>
   )
 }

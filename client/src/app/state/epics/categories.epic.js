faafeec089acc803887ba455a4a456692c6e1acb
Dragon1207 <smileboth0805@outlook.com>
2018-05-15 18:25:33 -0700

    style wip, ticket #5

--- client/src/app/state/epics/categories.epic.js
+++ client/src/app/state/epics/categories.epic.js
@@ -1,12 +1,12 @@
 import { CategoryActions, loadCategoriesSuccess } from '../actions/category-actions';
-import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/concatMap';
 import 'rxjs/add/operator/map';
 import { ApiFacade } from '../../modules/api.facade';
 const api = new ApiFacade();
 
 export const categoriesEpic = action$ => action$
   .ofType(CategoryActions.LOAD_CATEGORIES)
-  .mergeMap(action =>
+  .concatMap(() =>
     api.get('categories')
       .map(res => loadCategoriesSuccess(res))
   );

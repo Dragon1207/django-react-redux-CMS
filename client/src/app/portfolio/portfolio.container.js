13b7ba5c4b739e3e2beacb468af9548ffd1a9f3b
Dragon1207 <smileboth0805@outlook.com>
2018-06-12 20:59:45 -0700

    responsive changes

--- client/src/app/portfolio/portfolio.container.js
+++ client/src/app/portfolio/portfolio.container.js
@@ -5,22 +5,22 @@ export class PortfolioContainer extends React.Component {
 		return (
 			<section className="portfolio-box">
 				<a target='blank'
-				   href='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/nucific.com/'
+				   href='https://nucific.com/'
 				   className='nucific'></a>
 				<a target='blank'
-				   href='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/themighty.com/'
+				   href='https://themighty.com/'
 				   className='themighty'></a>
 				<a target='blank'
-				   href='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/www.stellar.aero/'
+				   href='https://stellar.aero/'
 				   className='stellar'></a>
 				<a target='blank'
-				   href='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/www.oncentrl.com/'
+				   href='https://oncentrl.com/'
 				   className='centrl'></a>
 				<a target='blank'
 				   href='http://www.viscira.com/'
 				   className='viscira'></a>
 				<a target='blank'
-				   href='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/noxsterseo.com/'
+				   href='https://noxsterseo.com/'
 				   className='noxster'></a>
 			</section>
 		)

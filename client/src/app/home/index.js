6ae03d0f52f4ffa3810573dbcdadce09bce5b85c
Dragon1207 <smileboth0805@outlook.com>
2018-06-13 00:35:06 -0700

    responsiveness wip

--- client/src/app/home/index.js
+++ client/src/app/home/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
 export function Home() {
-  return (
-    <section className={'home flex'} >
-      <div className={'home-text flex-6'}>
-        <h1><span className={'dancing-r'}>r</span>afay</h1>
-        <h2>
-          <i>tech enthusiast. musician.
-          </i>
-        </h2>
-        <div className={'home-about-text'}>
-          I like to build things that make people's lives easier. Web developer for over 9 years, wizard of style. Contact me if you would like to get started on a project or consulting.
+	return (
+		<section className={'home flex'}>
+			<div className={'home-text flex-6'}>
+				<h1>rafay</h1>
+				<h2>
+					<i>
+						Creative. Bold. Outlier.
+					</i>
+				</h2>
+				<div className={'home-about-text'}>
+					Contact me if you would like consultation on project development or goals.
 
-          <div className={'contact-info'}>
-            <div className={'email-icon'}>
-              <i className={'material-icons'}>email</i>
-              <span><a href={'mailto:rafay826@gmail.com'}>rafay826@gmail.com</a></span>
-            </div>
-            <a className={'github-icon'}
-               target={'blank'}
-               href={'https://github.com/rafay826'}>
-              <img className={'github-img'} alt={'github'} src='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/github-icon.svg'/>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+					<div className={'contact-info'}>
+						<div className={'email-icon'}>
+							<i className={'material-icons'}>email</i>
+							<span><a href={'mailto:rafay826@gmail.com'}>rafay826@gmail.com</a></span>
+						</div>
+						<a className={'github-icon'}
+						   target={'blank'}
+						   href={'https://github.com/rafay826'}>
+							<img className={'github-img'} alt={'github'}
+							     src='https://rafayio-assets.nyc3.digitaloceanspaces.com/rafay.io/assets/media/github-icon.svg'/>
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
